920325558241edc29cb76a03f917a5a586b7f54d
Jose Munoz <jdmg93@gmail.com>
2020-10-12 18:34:54 -0700

    mobile header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,11 +9,13 @@ const Header = () => (
       pointerEvents: "none",
       touchEvents: "none",
       width: "100vw",
+      height: '100vh',
     }}
   >
     <div className={styles.typewriter}>
       <h1>José Muñoz: Señor Engineer</h1>
     </div>
+    <h1 className={styles.mobileHeader}>José Muñoz</h1>
     <div className={styles.actionButton} style={{ pointerEvents: "auto" }}>
       <Link href="/about">Learn More</Link>
     </div>
